81232486b69d4c0168fb6254bced8303e2d0ee11
Alexander Rehmer <alexander.rehmer@gmail.com>
2023-11-02 16:26:43 +0100

    updated segmentation procedures

---
+++ hspytools/clust/gk.py
@@ -0,0 +1,82 @@
+import numpy as np
+from scipy.linalg import norm
+
+
+class GK:
+    def __init__(self, n_clusters=4, max_iter=100, m=2, error=1e-6):
+        super().__init__()
+        self.u, self.centers, self.f = None, None, None
+        self.clusters_count = n_clusters
+        self.max_iter = max_iter
+        self.m = m
+        self.error = error
+
+    def fit(self, z):
+        N = z.shape[0]
+        C = self.clusters_count
+        centers = []
+
+        u = np.random.dirichlet(np.ones(N), size=C)
+
+        iteration = 0
+        while iteration < self.max_iter:
+            u2 = u.copy()
+
+            centers = self.next_centers(z, u)
+            f = self._covariance(z, centers, u)
+            dist = self._distance(z, centers, f)
+            u = self.next_u(dist)
+            iteration += 1
+
+            # Stopping rule
+            if norm(u - u2) < self.error:
+                break
+
+        self.f = f
+        self.u = u
+        self.centers = centers
+        return centers
+
+    def next_centers(self, z, u):
+        um = u ** self.m
+        return ((um @ z).T / um.sum(axis=1)).T
+
+    def _covariance(self, z, v, u):
+        um = u ** self.m
+
+        denominator = um.sum(axis=1).reshape(-1, 1, 1)
+        temp = np.expand_dims(z.reshape(z.shape[0], 1, -1) - v.reshape(1, v.shape[0], -1), axis=3)
+        temp = np.matmul(temp, temp.transpose((0, 1, 3, 2)))
+        numerator = um.transpose().reshape(um.shape[1], um.shape[0], 1, 1) * temp
+        numerator = numerator.sum(0)
+
+        return numerator / denominator
+
+    def _distance(self, z, v, f):
+        dif = np.expand_dims(z.reshape(z.shape[0], 1, -1) - v.reshape(1, v.shape[0], -1), axis=3)
+        determ = np.power(np.linalg.det(f), 1 / self.m)
+        det_time_inv = determ.reshape(-1, 1, 1) * np.linalg.pinv(f)
+        temp = np.matmul(dif.transpose((0, 1, 3, 2)), det_time_inv)
+        output = np.matmul(temp, dif).squeeze().T
+        return np.fmax(output, 1e-8)
+
+    def next_u(self, d):
+        power = float(1 / (self.m - 1))
+        d = d.transpose()
+        denominator_ = d.reshape((d.shape[0], 1, -1)).repeat(d.shape[-1], axis=1)
+        denominator_ = np.power(d[:, None, :] / denominator_.transpose((0, 2, 1)), power)
+        denominator_ = 1 / denominator_.sum(1)
+        denominator_ = denominator_.transpose()
+
+        return denominator_
+
+    def predict(self, z):
+        if len(z.shape) == 1:
+            z = np.expand_dims(z, axis=0)
+
+        dist = self._distance(z, self.centers, self.f)
+        if len(dist.shape) == 1:
+            dist = np.expand_dims(dist, axis=0)
+
+        u = self.next_u(dist)
+        return np.argmax(u, axis=0)
