4497d03a03630a55bfea890219fde6798a936674
ChristianHeimannSensor <161726575+ChristianHeimannSensor@users.noreply.github.com>
2024-04-04 13:55:25 +0200

    updated test_160x120

--- hspytools/tests/test_importBCC_160x120.py
+++ hspytools/tests/test_importBCC_160x120.py
@@ -5,26 +5,15 @@ Created on Wed Apr  3 09:07:54 2024
 @author: Rehmer
 """
 
-# import pandas as pd
-from pathlib import Path
-# import numpy as np
-# import h5py
-import matplotlib.pyplot as plt
-import pickle as pkl
-from hsfit.cmap_mgr import cmap_mgr
 
-# # %% Test implementation
-from hspytools.readers import HTPAdGUI_FileReader
+from pathlib import Path
+
 from hspytools.tparray import TPArray
 
 # %% Specify paths
-bcc_47 = Path.cwd() / 'data'  / 'bcc' / '02042024_ 132630____47.BCC'
+bcc_47 = Path.cwd() / 'BCCs'  / '160x120_L10k0_F0k72_ID47.BCC'
 
-# meas_path = Path.cwd() / 'data' / 'LaudaBB' / 'meas' 
 
-# Toref_path = Path.cwd() / 'data' / 'LaudaBB' / 'ref' / 'Versuchsplan_160x120_L10k0_F0k72_LaudaBB.csv'
-
-# cmap_path = Path.cwd() / 'cmap_160x120_L10k0_LaudaBB.pkl'
 
 # %% 
 
